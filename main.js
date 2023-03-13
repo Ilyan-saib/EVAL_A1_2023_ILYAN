025fb3edeebddcf23a88152921bf1e92d0225268
CannelleJP <isaibmezghiche@edenschool.fr>
2023-03-13 16:06:01 +0100

    finish object data

---
+++ main.js
@@ -0,0 +1,57 @@
+let carte = {
+  1: [{
+   
+imgSrc: "/assets/img/img-01.jpg",
+title: "Lorem Ipsum",
+text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos placeat distincio id achitecto doloremque ipsum dolorem suscipit nemo !"
+
+}]
+,
+2: [{
+  
+  
+  imgSrc: "/assets/img/img-02.jpg",
+  title: "Lorem Ipsum",
+  text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos placeat distincio id achitecto doloremque ipsum dolorem suscipit nemo !"
+  
+  
+  
+   
+  }]
+  ,
+3: [{
+  
+  
+  imgSrc: "/assets/img/img-03.jpg",
+  title: "Lorem Ipsum",
+  text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos placeat distincio id achitecto doloremque ipsum dolorem suscipit nemo !"
+  
+  
+  
+   
+  }]
+  ,
+  4: [{
+    
+    
+    imgSrc: "/assets/img/img-04.jpg",
+    title: "Lorem Ipsum",
+    text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos placeat distincio id achitecto doloremque ipsum dolorem suscipit nemo !"
+    
+    
+    
+     
+    }]
+    ,
+    5: [{
+      
+      
+      imgSrc: "/assets/img/img-05.jpg",
+      title: "Lorem Ipsum",
+      text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos placeat distincio id achitecto doloremque ipsum dolorem suscipit nemo !"
+      
+      
+      
+       
+      }]
+}
