e8317e968c1ee7bfaf37d0201b26955a2f568106
CannelleJP <isaibmezghiche@edenschool.fr>
2023-03-14 16:59:26 +0100

    finish footer

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let carte = {
-  1: [{
+let carte =        [{
+  1: {
    
 imgSrc: "/assets/img/img-01.jpg",
 title: "Lorem Ipsum",
 text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos placeat distincio id achitecto doloremque ipsum dolorem suscipit nemo !"
 
-}]
+}
 ,
-2: [{
+2: {
   
   
   imgSrc: "/assets/img/img-02.jpg",
@@ -17,9 +17,9 @@ text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos
   
   
    
-  }]
+  }
   ,
-3: [{
+3: {
   
   
   imgSrc: "/assets/img/img-03.jpg",
@@ -29,9 +29,9 @@ text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos
   
   
    
-  }]
+  }
   ,
-  4: [{
+  4: {
     
     
     imgSrc: "/assets/img/img-04.jpg",
@@ -41,9 +41,9 @@ text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos
     
     
      
-    }]
+    }
     ,
-    5: [{
+    5: {
       
       
       imgSrc: "/assets/img/img-05.jpg",
@@ -53,24 +53,10 @@ text: "lorem ipsum dolor sit , amet consectetur adipiscing elit. Voluptatem quos
       
       
        
-      }]
+      }
+}]
+
+
+for (const item of carte) {
+  console.log(carte);
 }
-
-
-// for (const item in carte) {
-
-
-// console.log(carte.length);
-
-// let sect_img = document.getElementById(`img${1}`)
-// let sect_title = document.getElementById(`title${1}`)
-// let sect_desc = document.getElementById(`desc${1}`)
-
-// console.log();
-
-// sect_img.src = carte[1][0].imgSrc
-// sect_title.innerText = carte[1][0].title
-// sect_desc.innerText = carte[1][0].text
-
-
-// }
